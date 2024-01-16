14da0dda4069d77ff2e7818de5734feb77cd7816
Carolina Teixeira <maria.silva@creathus.org.br>
2024-01-15 21:25:49 -0400

    feat: task creation route

--- src/routes.js
+++ src/routes.js
@@ -1,3 +1,4 @@
+import { randomUUID } from 'node:crypto';
 import { Database } from './database.js';
 import { buildRoutePath } from './utils/build-route-path.js';
 
@@ -16,6 +17,26 @@ export const routes = [
       } : null);
 
       return res.end(JSON.stringify(tasks));
+    }
+  },
+  {
+    method: 'POST',
+    path: buildRoutePath('/tasks'),
+    handler: (req, res) => {
+      const { title, description } = req.body;
+      
+      const task = {
+        id: randomUUID(),
+        title,
+        description,
+        completed_at: null,
+        created_at: new Date(),
+        updated_at: new Date(),
+      }
+
+      database.insert('tasks', task);
+      
+      return res.writeHead(201).end();
     }
   }
 ]
