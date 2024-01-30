fbb6851b32d31ab099273c77bf6ff87cd8b65f31
Carolina Teixeira <maria.silva@creathus.org.br>
2024-01-30 16:49:00 -0400

    perf: structuring and applying asynchronism

--- streams/stream-csv.js
+++ streams/stream-csv.js
@@ -1,15 +1,22 @@
 import fs from 'node:fs';
 import { parse } from 'csv-parse';
 
-fs
-  .createReadStream('./streams/tasks.csv')
-  .pipe(parse({
-    delimiter: ',',
-    fromLine: 2
-  })).on('data', (data) => {
-    const [title, description] = data
 
-    fetch('http://localhost:3333/tasks', {
+async function importTasks() {
+  const stream = fs.createReadStream('./streams/tasks.csv')
+    .pipe(parse({
+      delimiter: ',',
+      fromLine: 2
+    }))
+
+  console.log('Iniciando importação...⌛');
+
+  for await (const line of stream) {
+    const [title, description] = line
+
+    console.log(`\tCadastrando tarefa: ${title}...⬆️`);
+
+    await fetch('http://localhost:3333/tasks', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -19,5 +26,9 @@ fs
         description
       })
     })
-  })
+  }
 
+  console.log('Importação concluída!✅');
+}
+
+importTasks()
